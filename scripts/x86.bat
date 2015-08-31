72f5fd27e024285b02033c02287e474b5cf6925f
774 <774>
2015-08-31 21:53:57 +0900

    インストーラーをダウンロードした後の改行コード変換を追加

--- scripts/x86.bat
+++ scripts/x86.bat
@@ -10,12 +10,15 @@ if not defined INSTALLER_URL (
   set INSTALLER_URL=https://raw.githubusercontent.com/neguse11/cm3d2_plugins_okiba/%OKIBA_BRANCH%/scripts/installer.bat
 )
 set INSTALLER_FILE=installer.bat
+set INSTALLER_TMP=%INSTALLER_FILE%.tmp
 
-powershell -Command "(New-Object Net.WebClient).DownloadFile('%INSTALLER_URL%', '%ROOT%\%INSTALLER_FILE%')"
-if not exist "%ROOT%\%INSTALLER_FILE%" (
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%INSTALLER_URL%', '%ROOT%\%INSTALLER_TMP%')"
+if not exist "%ROOT%\%INSTALLER_TMP%" (
   echo インストーラーのダウンロードに失敗しました
   exit /b 1
 )
+type "%ROOT%\%INSTALLER_TMP%" | more /p > "%ROOT%\%INSTALLER_FILE%"
+del "%ROOT%\%INSTALLER_TMP%"
 
 call "%ROOT%\%INSTALLER_FILE%"
 del "%ROOT%\%INSTALLER_FILE%"
