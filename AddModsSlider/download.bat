697f0c37fb7415e18637d603425090c16a830d7a
774 <774>
2015-09-10 21:06:45 +0900

    AddModsSliderのコードをgithubから取るように変更

--- AddModsSlider/download.bat
+++ AddModsSlider/download.bat
@@ -1,8 +1,7 @@
 @echo off && setlocal ENABLEEXTENSIONS && pushd "%~dp0" && call "%~dp0..\scripts\base.bat" || exit /b %ERRORLEVEL%
 
-@rem githubからダウンロードしたいが、現時点のコードはコンパイルできないため保留
-@rem set SRC_URL=https://raw.githubusercontent.com/CM3D2-01/CM3D2.AddModsSlider.Plugin/master/CM3D2.AddModsSlider.Plugin.cs
-set SRC_URL=http://pastebin.com/raw.php?i=wteDMDQL
+set SRC_URL=https://raw.githubusercontent.com/CM3D2-01/CM3D2.AddModsSlider.Plugin/c26e907c9b9d0f0aa606e721586be8a90689a005/CM3D2.AddModsSlider.Plugin.cs
+@rem set SRC_URL=http://pastebin.com/raw.php?i=wteDMDQL
 set SRC_FILE=CM3D2.AddModsSlider.Plugin.cs
 
 echo 「%SRC_URL%」から「%SRC_FILE%」をダウンロードします
