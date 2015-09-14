8cfadac8fbaf401d751c63bb863dc70cca510dd6
774 <774>
2015-09-14 18:44:24 +0900

    AddModsSliderのハンドル部PNGのコピー忘れを修正

--- AddModsSlider/compile.bat
+++ AddModsSlider/compile.bat
@@ -9,4 +9,5 @@ call "%~dp0..\scripts\csc-compile.bat" || exit /b 1
 
 mkdir "%UNITY_INJECTOR_DIR%\Config\" >nul 2>&1
 copy ModsParam.xml "%UNITY_INJECTOR_DIR%\Config\" >nul 2>&1 || ( echo ファイルのコピーに失敗しました && exit /b 1 )
+copy ModsSliderWin.png "%UNITY_INJECTOR_DIR%\Config\" >nul 2>&1 || ( echo ファイルのコピーに失敗しました && exit /b 1 )
 popd
