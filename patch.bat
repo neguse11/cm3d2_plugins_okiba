176f0479e597340385cf5d750801822a58d10c4c
774 <774>
2015-09-18 20:10:07 +0900

    パッチ処理の前に全DLLをコピーするように変更(その３>>347)

--- patch.bat
+++ patch.bat
@@ -2,7 +2,7 @@
 
 pushd "%REIPATCHER_DIR%"
 del /q "%CM3D2_MOD_MANAGED_DIR%\Assembly-CSharp.dll.*.bak" >nul 2>&1
-copy /y "%CM3D2_VANILLA_MANAGED_DIR%\Assembly-CSharp.dll" "%CM3D2_MOD_MANAGED_DIR%" >nul 2>&1 || ( echo ファイルのコピーに失敗しました && exit /b 1 )
+copy /y "%CM3D2_VANILLA_MANAGED_DIR%\*.dll" "%CM3D2_MOD_MANAGED_DIR%" >nul 2>&1 || ( echo ファイルのコピーに失敗しました && exit /b 1 )
 if not exist "%REIPATCHER_INI%" (
   echo.&echo ReiPatcherの設定ファイル %REIPATCHER_DIR%\%REIPATCHER_INI% が存在しません
   goto error
